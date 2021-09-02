68c692735cf6c8cd88ca7c16059292be2ec0bd6c
Asad Z <essayyzed@gmail.com>
2021-09-02 15:14:32 +0500

    Added the camera sensor to the vehicle

--- 00-carla-tutorial.py
+++ 00-carla-tutorial.py
@@ -20,6 +20,11 @@ try:
 except IndexError:
     pass
 
+
+# Setting the image size of camera image
+IM_WIDTH = 640      
+IM_HEIGHT = 480
+
 actor_list = (
     []
 )  # List of actors used in the simulation, will make easy for us to deal with actors
@@ -56,12 +61,25 @@ try:
     )  # driving the car with the speed of 2.0 in a straight direction
 
     actor_list.append(vehicle)   # appending our car actor to a list so that at the end of simulation we can destroy it
+    
+    camera_bp = blueprint_library.find("sensor.camera.rgb")
+    camera_bp.set_attribute("image_size_x", f"{IM_WIDTH}")
+    camera_bp.set_attribute("image_size_y", f"{IM_HEIGHT}")
+    camera_bp.set_attribute("fov", "110")  # setting the field of view
+    
+    spawn_point = carla.Transform(carla.Location(x=2.5, z=0.7))     # point where to fix the camera
+    sensor = world.spawn_actor(camera_bp, spawn_point, attach_to=vehicle)   # passing the attributes set above for the sensor
+    actor_list.append(sensor)
+    
+    sensor.listen(lambda data: process_img(data))
+    
+    
     time.sleep(10)
 finally:
 
     """
     After we are done with our task we need to clear up every thing using the builtin
-    function `actor.destroy()
+    function `actor.destroy()`
     """
     for actor in actor_list:
         actor.destroy()
