6696f81ae8b25c54cdaeb5bdb31f1c0a9307b680
Asad Z <essayyzed@gmail.com>
2021-09-02 13:13:04 +0500

    Vehicle Spawned and Driven

--- 00-carla-tutorial.py
+++ 00-carla-tutorial.py
@@ -27,7 +27,7 @@ actor_list = (
 try:
     client = carla.Client(
         "localhost", 2000
-    )  # Means CARLA will run on Local machine. Some public ip in case of remote server which is not recommanded
+    )  # Means CARLA will run on Local machine. Some public ip in case of remote server which is not recommended
 
     client.set_timeout(30)  # Defines for how much time the simulation should run
 
@@ -35,15 +35,28 @@ try:
         client.get_world()
     )  # consider it an abstract layer where all the simulations will take place
 
-    blueprint_library = world.get_blueprint_library() #  this where we can import all of our actors etc
+    blueprint_library = (
+        world.get_blueprint_library()
+    )  #  this where we can import all of our actors etc
 
-    actor_car = blueprint_library.filter("model3")[0] # spawning `car actor` of tesla model3
+    actor_car = blueprint_library.filter("model3")[
+        0
+    ]  # spawning `car actor` of tesla model3
     print(actor_car)
-    
-    spawn_point = world.get_map().get_spawn_points()[0]  # specfiying where the vehicle should be spawned
 
+    spawn_point = world.get_map().get_spawn_points()[
+        0
+    ]  # specifying where the vehicle should be spawned
 
+    vehicle = world.spawn_actor(
+        actor_car, spawn_point
+    )  # spawn the vehicle actor at the spawn point specified
+    vehicle.apply_control(
+        carla.VehicleControl(throttle=2.0, steer=0.0)
+    )  # driving the car with the speed of 2.0 in a straight direction
 
+    actor_list.append(vehicle)   # appending our car actor to a list so that at the end of simulation we can destroy it
+    time.sleep(10)
 finally:
 
     """
